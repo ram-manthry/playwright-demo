e1bbe45243b4cdae2da3662b75c5b16834ef9f8c
Ram Manthry <ram.manthry@gmail.com>
2024-03-23 13:37:37 +1300

    small refactor, moved trim to outside expect

--- tests/dk-demo.spec.ts
+++ tests/dk-demo.spec.ts
@@ -3,9 +3,8 @@ import { test, expect } from '@playwright/test';
 test.skip('test', async ({ page }) => {
 	const expected = 'Global Indian!';
 	await page.goto('https://www.magicbricks.com/');
-	const actual = await page
-		.locator('.mb-search__heading')
-		.locator('span')
-		.textContent();
-	expect(actual?.trim()).toEqual(expected);
+	const actual = (
+		await page.locator('.mb-search__heading').locator('span').textContent()
+	)?.trim();
+	expect(actual).toEqual(expected);
 });
