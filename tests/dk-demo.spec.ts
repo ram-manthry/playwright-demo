77a76c1b4f76dcddf1fcde09eac5d83c4cf09a6b
Ram Manthry <ram.manthry@gmail.com>
2024-03-23 13:55:55 +1300

    introduced page url variable

--- tests/dk-demo.spec.ts
+++ tests/dk-demo.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test.skip('test', async ({ page }) => {
 	const expected = 'Global Indian!';
-	await page.goto('https://www.magicbricks.com/');
+	const pageUrl = 'https://www.magicbricks.com/';
+	await page.goto(pageUrl);
 	const actual = (
 		await page.locator('.mb-search__heading').locator('span').textContent()
 	)?.trim();
